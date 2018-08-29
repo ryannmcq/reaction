e3e52a7c2ec952f9156b39434533127c3c333461
Ryann McQuilton <20259631+ryannmcq@users.noreply.github.com>
2018-08-29 15:10:49 -0700

    Update title

--- app/javascript/components/board/ToggleableListForm.jsx
+++ app/javascript/components/board/ToggleableListForm.jsx
@@ -13,7 +13,7 @@ export default class ToggleableListForm extends React.Component {
     return (
       <div id="new-list" className={"new-list " + (this.props.formOpen ? 'selected' : '')}>
         <span onClick={this.props.toggleForm}>Add a list...</span>
-        <input onChange={this.onChange} type="text" placeholder="Add a list..." value={this.props.inputText} />
+        <input type="text" onChange={this.onChange} type="text" placeholder="Add a list..." value={this.props.inputText} />
         <div>
           <input onClick={this.submitList} type="submit" className="button" value="Save" />
           <i onClick={this.props.onClose} className="x-icon icon"></i>
